6517dcd270b8cacb65079b028d34058d9d77c22a
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-26 23:43:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,12 @@ class Student
     DB[:conn].execute(sql, x)
   end
   
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    first_student_row = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
+  end
+  
   
   
   def save
