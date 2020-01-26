60096628b9a9edb1c6fc9bd1917ab9d48be1cf8c
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-26 23:36:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.all
     sql = "SELECT * FROM students"
     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    self.new_from_db(row)
   end
 
   def self.find_by_name(name)
